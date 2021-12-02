fee5d865f16ebd1e5c5b89ca581306506979ccc3
balarbirwa <95391628+balarbirwa@users.noreply.github.com>
2021-12-01 18:48:51 -0700

    Create match.h

---
+++ match.h
@@ -0,0 +1,18 @@
+//CSCI Fall 2021
+//Birwa Balar
+//Recitation 117 - Naga Sai Meenakshi Sistla
+//Project 3 - Match Class
+#include <iostream>
+#include <string>
+#ifndef Match_h
+#define Match_h
+using namespace std;
+
+class Match
+{
+    public:
+    //set match function
+    bool setMatch(int compatability);
+
+};
+#endif
